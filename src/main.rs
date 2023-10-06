a49fd451d671734994d5ee5de4ac910ab6d216d8
wongfei2009 <wongfei2009@gmail.com>
2023-10-06 13:08:27 +0800

    Return json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use axum::{extract::State, routing::get, Router, Server};
-use std::{
-    fmt::Write,
-    sync::{Arc, Mutex},
-};
+use axum::{extract::State, routing::get, Json, Router, Server};
+use std::sync::{Arc, Mutex};
 use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
@@ -10,7 +7,7 @@ async fn main() {
     let sys = Arc::new(Mutex::new(System::new_all()));
     let app_state = AppState { sys };
     let router = Router::new()
-        .route("/cpu", get(cpu_info))
+        .route("/api/cpu", get(cpu_info))
         .with_state(app_state);
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     println!("Listening on http://{}", server.local_addr());
@@ -22,12 +19,9 @@ struct AppState {
     sys: Arc<Mutex<System>>,
 }
 
-async fn cpu_info(State(state): State<AppState>) -> String {
-    let mut s = String::new();
+async fn cpu_info(State(state): State<AppState>) -> Json<Vec<f32>> {
     let mut sys = state.sys.lock().unwrap();
     sys.refresh_cpu();
-    for (i, cpu) in sys.cpus().iter().enumerate() {
-        writeln!(&mut s, "{}: {:.2}%", i, cpu.cpu_usage()).unwrap();
-    }
-    s
+    let cpu_usage: Vec<f32> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+    Json(cpu_usage)
 }
