5eedd798dcbde9b713bb7930096d640cdba4f28a
wongfei2009 <wongfei2009@gmail.com>
2023-10-06 11:13:40 +0800

    Add axum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use axum::{routing::get, Router, Server};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let router = Router::new().route("/", get(hello_world));
+    let server = Server::bind(&"0.0.0.0:7032".parse().unwrap())
+        .serve(router.into_make_service());
+    server.local_addr();
+    server.await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
 }
