ab980bc83d5a1df40ee9eaca9235e26b1db85f39
wongfei2009 <wongfei2009@gmail.com>
2023-10-06 11:15:58 +0800

    Print listening addresss

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     let router = Router::new().route("/", get(hello_world));
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap())
         .serve(router.into_make_service());
-    server.local_addr();
+    println!("Listening on http://{}", server.local_addr());
     server.await.unwrap();
 }
 
