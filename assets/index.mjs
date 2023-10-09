2fe16f9ea173a16e7ced14e05caedf96007b5a43
wongfei2009 <wongfei2009@gmail.com>
2023-10-09 16:11:03 +0800

    Add htm

--- assets/index.mjs
+++ assets/index.mjs
@@ -1,8 +1,24 @@
-import { h, Component, render } from 'https://esm.sh/preact';
+import { h, render } from 'https://esm.sh/preact';
+import htm from 'https://esm.sh/htm';
+
+const html = htm.bind(h);
+
+function App(props) {
+    return html`
+    <div>
+        ${props.cpus.map((cpu) => {
+        return html`
+            <div>
+                <p>${cpu.toFixed(2)}%</p>
+            </div>
+            `;
+    })}
+    </div>
+    `;
+}
 
 setInterval(async function () {
     let response = await fetch("/api/cpu");
     let data = await response.json();
-    const app = h('pre', {}, JSON.stringify(data, null, 2));
-    render(app, document.body);
+    render(html`<${App} cpus=${data}></${App}>`, document.body);
 }, 1000);
